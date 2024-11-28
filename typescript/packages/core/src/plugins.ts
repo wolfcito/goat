016d4c768bab8c4a4cd2118e19139e759d321079
Agustin Armellini Fischer <armellini13@gmail.com>
2024-11-28 18:34:21 +0100

    Update plugin and wallet docs

--- typescript/packages/core/src/plugins.ts
+++ typescript/packages/core/src/plugins.ts
@@ -8,6 +8,7 @@ import type { Chain, WalletClient } from "./wallets";
  * @param TWalletClient - The type of wallet client to support. Defaults to WalletClient for chain-agnostic plugins.
  * @param name - The name of the plugin.
  * @param supportsChain - A function that returns true if the plugin supports the given chain.
+ * @param supportsSmartWallets - A function that returns true if the plugin supports smart wallets.
  * @param getTools - A function that returns the tools provided by the plugin.
  */
 export interface Plugin<
