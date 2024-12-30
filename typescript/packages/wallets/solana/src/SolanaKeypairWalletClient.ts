ba3b09ae7a0bf80afd49ffd7d0bc23d51da2b66b
Agustin Armellini Fischer <armellini13@gmail.com>
2024-12-30 18:28:09 +0100

    Rollback send tx changes

--- typescript/packages/wallets/solana/src/SolanaKeypairWalletClient.ts
+++ typescript/packages/wallets/solana/src/SolanaKeypairWalletClient.ts
@@ -1,18 +1,19 @@
 import {
-    ComputeBudgetProgram,
     type Keypair,
-    PublicKey,
-    TransactionInstruction,
     TransactionMessage,
     VersionedTransaction,
 } from "@solana/web3.js";
 import nacl from "tweetnacl";
-import { type SolanWalletClientCtorParams, SolanaWalletClient } from "./SolanaWalletClient";
+import {
+    type SolanWalletClientCtorParams,
+    SolanaWalletClient,
+} from "./SolanaWalletClient";
 import type { SolanaTransaction } from "./types";
 
-export type SolanaKeypairWalletClientCtorParams = SolanWalletClientCtorParams & {
-    keypair: Keypair;
-};
+export type SolanaKeypairWalletClientCtorParams =
+    SolanWalletClientCtorParams & {
+        keypair: Keypair;
+    };
 
 export class SolanaKeypairWalletClient extends SolanaWalletClient {
     #keypair: Keypair;
@@ -29,7 +30,10 @@ export class SolanaKeypairWalletClient extends SolanaWalletClient {
 
     async signMessage(message: string) {
         const messageBytes = Buffer.from(message);
-        const signature = nacl.sign.detached(messageBytes, this.#keypair.secretKey);
+        const signature = nacl.sign.detached(
+            messageBytes,
+            this.#keypair.secretKey
+        );
         return {
             signature: Buffer.from(signature).toString("hex"),
         };
@@ -39,100 +43,41 @@ export class SolanaKeypairWalletClient extends SolanaWalletClient {
         instructions,
         addressLookupTableAddresses = [],
         accountsToSign = [],
-    }: SolanaTransaction): Promise<{ hash: string }> {
-        const ixComputeBudget = await this.getComputeBudgetInstructions(instructions, "mid");
-        const allInstructions = [
-            ixComputeBudget.computeBudgetLimitInstruction,
-            ixComputeBudget.computeBudgetPriorityFeeInstructions,
-            ...instructions,
-        ];
-        const messageV0 = new TransactionMessage({
+    }: SolanaTransaction) {
+        const latestBlockhash = await this.connection.getLatestBlockhash();
+
+        const message = new TransactionMessage({
             payerKey: this.#keypair.publicKey,
-            recentBlockhash: ixComputeBudget.blockhash,
-            instructions: allInstructions,
-        }).compileToV0Message(await this.getAddressLookupTableAccounts(addressLookupTableAddresses));
-        const transaction = new VersionedTransaction(messageV0);
+            recentBlockhash: latestBlockhash.blockhash,
+            instructions,
+        }).compileToV0Message(
+            await this.getAddressLookupTableAccounts(
+                addressLookupTableAddresses
+            )
+        );
+        const transaction = new VersionedTransaction(message);
+
         transaction.sign([this.#keypair, ...accountsToSign]);
 
-        const timeoutMs = 90000;
-        const startTime = Date.now();
-        while (Date.now() - startTime < timeoutMs) {
-            const transactionStartTime = Date.now();
+        const hash = await this.connection.sendTransaction(transaction, {
+            maxRetries: 10,
+            preflightCommitment: "confirmed",
+        });
 
-            const hash = await this.connection.sendTransaction(transaction, {
-                maxRetries: 0,
-                skipPreflight: true,
-            });
+        await this.connection.confirmTransaction(
+            {
+                blockhash: latestBlockhash.blockhash,
+                lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+                signature: hash,
+            },
+            "confirmed"
+        );
 
-            const statuses = await this.connection.getSignatureStatuses([hash]);
-            if (statuses.value[0]) {
-                if (!statuses.value[0].err) {
-                    return { hash };
-                }
-            }
-
-            const elapsedTime = Date.now() - transactionStartTime;
-            const remainingTime = Math.max(0, 1000 - elapsedTime);
-            if (remainingTime > 0) {
-                await new Promise((resolve) => setTimeout(resolve, remainingTime));
-            }
-        }
-        throw new Error("Transaction timeout");
-    }
-
-    private priorityFeeTiers = {
-        min: 0.01,
-        mid: 0.5,
-        max: 0.95,
-    };
-
-    private async getComputeBudgetInstructions(
-        instructions: TransactionInstruction[],
-        feeTier: keyof typeof this.priorityFeeTiers,
-    ): Promise<{
-        blockhash: string;
-        computeBudgetLimitInstruction: TransactionInstruction;
-        computeBudgetPriorityFeeInstructions: TransactionInstruction;
-    }> {
-        try {
-            const blockhash = (await this.connection.getLatestBlockhash()).blockhash;
-            const messageV0 = new TransactionMessage({
-                payerKey: this.#keypair.publicKey,
-                recentBlockhash: blockhash,
-                instructions: instructions,
-            }).compileToV0Message();
-            const transaction = new VersionedTransaction(messageV0);
-            const simulatedTx = this.connection.simulateTransaction(transaction);
-            const estimatedComputeUnits = (await simulatedTx).value.unitsConsumed;
-            const safeComputeUnits = Math.ceil(
-                estimatedComputeUnits ? Math.max(estimatedComputeUnits + 100000, estimatedComputeUnits * 1.2) : 200000,
-            );
-            const computeBudgetLimitInstruction = ComputeBudgetProgram.setComputeUnitLimit({
-                units: safeComputeUnits,
-            });
-
-            const priorityFee = await this.connection
-                .getRecentPrioritizationFees()
-                .then(
-                    (fees) =>
-                        fees.sort((a, b) => a.prioritizationFee - b.prioritizationFee)[
-                            Math.floor(fees.length * this.priorityFeeTiers[feeTier])
-                        ].prioritizationFee,
-                );
-
-            const computeBudgetPriorityFeeInstructions = ComputeBudgetProgram.setComputeUnitPrice({
-                microLamports: priorityFee,
-            });
-
-            return {
-                blockhash,
-                computeBudgetLimitInstruction,
-                computeBudgetPriorityFeeInstructions,
-            };
-        } catch (error) {
-            throw new Error(`Failed to get compute budget instructions: ${error}`);
-        }
+        return {
+            hash,
+        };
     }
 }
 
-export const solana = (params: SolanaKeypairWalletClientCtorParams) => new SolanaKeypairWalletClient(params);
+export const solana = (params: SolanaKeypairWalletClientCtorParams) =>
+    new SolanaKeypairWalletClient(params);
