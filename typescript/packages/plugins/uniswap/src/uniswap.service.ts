c0ce5e6645db67981632c0445541244c380da7c2
Agustin Armellini Fischer <armellini13@gmail.com>
2025-03-11 14:03:10 +0100

    Fix uniswap and erc20 plugins

--- typescript/packages/plugins/uniswap/src/uniswap.service.ts
+++ typescript/packages/plugins/uniswap/src/uniswap.service.ts
@@ -8,7 +8,7 @@ export class UniswapService {
 
     private async makeRequest(endpoint: string, parameters: unknown) {
         const url = new URL(`${this.params.baseUrl}/${endpoint}`);
-
+        
         const response = await fetch(url.toString(), {
             method: "POST",
             body: JSON.stringify(parameters),
@@ -18,7 +18,9 @@ export class UniswapService {
         });
 
         if (!response.ok) {
-            throw new Error(`Failed to fetch ${endpoint}: ${JSON.stringify(await response.json(), null, 2)}`);
+            throw new Error(
+                `Failed to fetch ${endpoint}: ${JSON.stringify(await response.json(), null, 2)}`,
+            );
         }
 
         return response.json();
@@ -29,7 +31,10 @@ export class UniswapService {
         description:
             "Check if the wallet has enough approval for a token and return the transaction to approve the token. The approval must takes place before the swap transaction",
     })
-    async checkApproval(walletClient: EVMWalletClient, parameters: CheckApprovalBodySchema) {
+    async checkApproval(
+        walletClient: EVMWalletClient,
+        parameters: CheckApprovalBodySchema,
+    ) {
         const data = await this.makeRequest("check_approval", {
             token: parameters.token,
             amount: parameters.amount,
@@ -61,36 +66,59 @@ export class UniswapService {
         name: "uniswap_get_quote",
         description: "Get the quote for a swap",
     })
-    async getQuote(walletClient: EVMWalletClient, parameters: GetQuoteParameters) {
-        return this.makeRequest("quote", {
+    async getQuote(
+        walletClient: EVMWalletClient,
+        parameters: GetQuoteParameters,
+    ) {
+        return await this.makeRequest("quote", {
             ...parameters,
             tokenInChainId: walletClient.getChain().id,
-            tokenOutChainId: parameters.tokenOutChainId ?? walletClient.getChain().id,
+            tokenOutChainId:
+                parameters.tokenOutChainId ?? walletClient.getChain().id,
             swapper: walletClient.getAddress(),
         });
     }
 
     @Tool({
         name: "uniswap_swap_tokens",
-        description: "Swap tokens on Uniswap",
+        description:
+            "Swap tokens on Uniswap. Make sure to check the approval with the uniswap_check_approval tool before calling this tool. No need to call uniswap_get_quote before calling this tool.",
     })
-    async getSwapTransaction(walletClient: EVMWalletClient, parameters: GetQuoteParameters) {
-        const quote = await this.getQuote(walletClient, parameters);
+    async getSwapTransaction(
+        walletClient: EVMWalletClient,
+        parameters: GetQuoteParameters,
+    ) {
+        const { quote, permitData } = await this.getQuote(walletClient, parameters);
+
+        let signature: string | undefined;
+
+        if (permitData) {
+            signature = (
+                await walletClient.signTypedData({
+                    domain: permitData.domain,
+                    types: permitData.types,
+                    primaryType: "PermitSingle",
+                    message: permitData.values,
+                })
+            ).signature;
+        }
 
         const response = await this.makeRequest("swap", {
-            quote: quote.quote,
+            signature: signature,
+            quote: quote,
+            permitData: permitData ?? undefined,
         });
 
         const swap = response.swap;
 
-        const transaction = await walletClient.sendTransaction({
+        const tx = await walletClient.sendTransaction({
             to: swap.to,
             value: swap.value,
             data: swap.data,
         });
 
         return {
-            txHash: transaction.hash,
+            txHash: tx.hash,
         };
     }
 }
